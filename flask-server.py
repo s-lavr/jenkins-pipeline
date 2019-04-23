6baefbe54cdd982ef45d88f27e7a33e4accd9dc9
s-lavr <serg_lavr@hotmail.com>
2019-04-23 17:24:13 +0300

    check

--- flask-server.py
+++ flask-server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1> Completely new test message!</h1>"
+    return "<h1> Completely new test message!111</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
