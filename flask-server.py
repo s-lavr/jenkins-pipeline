6725d90b8e81a27ee5b94986ea0b5a327802008b
s-lavr <serg_lavr@hotmail.com>
2019-04-30 10:58:13 +0300

    Commit for PR

--- flask-server.py
+++ flask-server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Test message for New branch</h1>"
+    return "<h1>New message specially for PR</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
