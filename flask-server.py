936f2cafd917dc98d9c7b92a5b6d960b9d9d3aee
s-lavr <serg_lavr@hotmail.com>
2019-05-02 14:44:31 +0300

    Additional changes to programm
    Added new path and text for page

--- flask-server.py
+++ flask-server.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "<h1>New message specially for branch</h1>"
 
+@app.route('/test')
+def hello():
+    return "<h1>Additional message (different path)</h1>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
