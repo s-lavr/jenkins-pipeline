1d480c687b5ea6d899243f2891a1a35edf33603f
s-lavr <serg_lavr@hotmail.com>
2019-05-08 12:33:14 +0300

    Changed message in flask-server

--- flask-server.py
+++ flask-server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1> New message to test correct deploying of new chart </h1>"
+    return "<h1> Test message #1 </h1>"
 
 @app.route('/test')
 def test():
